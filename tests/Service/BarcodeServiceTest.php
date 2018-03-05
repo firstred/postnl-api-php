8bf00992999b45593d323f13747e3a0de6bce542
Michael Dekker <git@michaeldekker.nl>
2018-03-05 18:45:49 +0100

    Add basic tests

---
+++ tests/Service/BarcodeServiceTest.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Thirty Development, LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @author    Michael Dekker <michael@thirtybees.com>
+ * @copyright 2017 Thirty Development, LLC
+ * @license   https://opensource.org/licenses/MIT The MIT License
+ */
+
+namespace ThirtyBees\PostNL\Tests\Service;
+
+use ThirtyBees\PostNL\Entity\Address;
+use ThirtyBees\PostNL\Entity\Customer;
+use ThirtyBees\PostNL\Entity\SOAP\UsernameToken;
+use ThirtyBees\PostNL\PostNL;
+
+class BarcodeServiceTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var PostNL $postnl */
+    protected $postnlXml;
+    /** @var PostNL $postnlRest */
+    protected $postnlRest;
+
+    /**
+     * @before
+     * @throws \ThirtyBees\PostNL\Exception\InvalidArgumentException
+     * @throws \ReflectionException
+     */
+    public function setupPostNL()
+    {
+        $this->postnlXml = new PostNL(
+            Customer::create()
+                ->setCollectionLocation('123456')
+                ->setCustomerCode('DEVC')
+                ->setCustomerNumber('11223344')
+                ->setContactPerson('Test')
+                ->setAddress(Address::create([
+                    'AddressType' => '02',
+                    'City'        => 'Hoofddorp',
+                    'CompanyName' => 'PostNL',
+                    'Countrycode' => 'NL',
+                    'HouseNr'     => '42',
+                    'Street'      => 'Siriusdreef',
+                    'Zipcode'     => '2132WT',
+                ]))
+                ->setGlobalPackBarcodeType('AB')
+                ->setGlobalPackCustomerCode('1234')
+            , new UsernameToken(null, 'test'),
+            true,
+            PostNL::MODE_SOAP
+        );
+
+        $this->postnlRest = new PostNL(
+            Customer::create()
+                ->setCollectionLocation('123456')
+                ->setCustomerCode('DEVC')
+                ->setCustomerNumber('11223344')
+                ->setContactPerson('Test')
+                ->setAddress(Address::create([
+                    'AddressType' => '02',
+                    'City'        => 'Hoofddorp',
+                    'CompanyName' => 'PostNL',
+                    'Countrycode' => 'NL',
+                    'HouseNr'     => '42',
+                    'Street'      => 'Siriusdreef',
+                    'Zipcode'     => '2132WT',
+                ]))
+                ->setGlobalPackBarcodeType('AB')
+                ->setGlobalPackCustomerCode('1234')
+            , new UsernameToken(null, 'test'),
+            true,
+            PostNL::MODE_REST
+        );
+    }
+
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
