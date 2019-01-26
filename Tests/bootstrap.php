ebb9023606dfe1ac2a7a19945c8a92592f3470bd
Michael Dekker <git@michaeldekker.nl>
2019-01-26 08:16:08 +0100

    Add dotenv file to test suite

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -2,10 +2,26 @@
 declare(strict_types=1);
 require __DIR__.'/../vendor/autoload.php';
 
+use Dotenv\Dotenv;
+use Dotenv\Exception\InvalidPathException;
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
 use Psr\Log\LogLevel;
 use wappr\Logger;
+
+$dotenv = Dotenv::create(__DIR__.'/../');
+try {
+    $dotenv->load();
+} catch (InvalidPathException $e) {
+    // Do absolutely nothing with this error :D
+}
+$dotenv->required([
+    'POSTNL_API_KEY',
+    'POSTNL_COLLECTION_LOCATION',
+    'POSTNL_CONTACT_PERSION',
+    'POSTNL_CUSTOMER_CODE',
+    'POSTNL_CUSTOMER_NUMBER',
+]);
 
 $filesystemAdapter = new Local(__DIR__.'/');
 $filesystem = new Filesystem($filesystemAdapter);
