8a9a56652b10670f3fe0496fb0a782e41606d789
Michael Dekker <git@michaeldekker.nl>
2023-04-11 00:33:47 +0200

    Support `ProductOption` REST property aliases

--- src/Entity/ProductOption.php
+++ src/Entity/ProductOption.php
@@ -26,6 +26,8 @@
 
 namespace Firstred\PostNL\Entity;
 
+use Firstred\PostNL\Exception\InvalidArgumentException;
+use Firstred\PostNL\Exception\NotSupportedException;
 use Firstred\PostNL\Service\BarcodeService;
 use Firstred\PostNL\Service\ConfirmingService;
 use Firstred\PostNL\Service\DeliveryDateService;
@@ -39,8 +41,10 @@ use Firstred\PostNL\Service\TimeframeService;
  *
  * @method string|null   getCharacteristic()
  * @method string|null   getOption()
+ * @method string|null   getDescription()
  * @method ProductOption setCharacteristic(string|null $Characteristic = null)
  * @method ProductOption setOption(string|null $Option = null)
+ * @method ProductOption setDescription(string|null $Description = null)
  *
  * @since 1.0.0
  */
@@ -51,30 +55,37 @@ class ProductOption extends AbstractEntity
         'Barcode' => [
             'Characteristic' => BarcodeService::DOMAIN_NAMESPACE,
             'Option'         => BarcodeService::DOMAIN_NAMESPACE,
+            'Description'    => BarcodeService::DOMAIN_NAMESPACE,
         ],
         'Confirming' => [
             'Characteristic' => ConfirmingService::DOMAIN_NAMESPACE,
             'Option'         => ConfirmingService::DOMAIN_NAMESPACE,
+            'Description'    => ConfirmingService::DOMAIN_NAMESPACE,
         ],
         'Labelling' => [
             'Characteristic' => LabellingService::DOMAIN_NAMESPACE,
             'Option'         => LabellingService::DOMAIN_NAMESPACE,
+            'Description'    => LabellingService::DOMAIN_NAMESPACE,
         ],
         'DeliveryDate' => [
             'Characteristic' => DeliveryDateService::DOMAIN_NAMESPACE,
             'Option'         => DeliveryDateService::DOMAIN_NAMESPACE,
+            'Description'    => DeliveryDateService::DOMAIN_NAMESPACE,
         ],
         'Location' => [
             'Characteristic' => LocationService::DOMAIN_NAMESPACE,
             'Option'         => LocationService::DOMAIN_NAMESPACE,
+            'Description'    => LocationService::DOMAIN_NAMESPACE,
         ],
         'Timeframe' => [
             'Characteristic' => TimeframeService::DOMAIN_NAMESPACE,
             'Option'         => TimeframeService::DOMAIN_NAMESPACE,
+            'Description'    => TimeframeService::DOMAIN_NAMESPACE,
         ],
         'Shipping' => [
             'Characteristic' => ShippingService::DOMAIN_NAMESPACE,
             'Option'         => ShippingService::DOMAIN_NAMESPACE,
+            'Description'    => ShippingService::DOMAIN_NAMESPACE,
         ],
     ];
     // @codingStandardsIgnoreStart
@@ -82,17 +93,51 @@ class ProductOption extends AbstractEntity
     protected $Characteristic;
     /** @var string|null */
     protected $Option;
+    /** @var string|null */
+    protected $Description;
     // @codingStandardsIgnoreEnd
 
     /**
      * @param string|null $Characteristic
      * @param string|null $Option
+     * @param string|null $Description
      */
-    public function __construct($Characteristic = null, $Option = null)
+    public function __construct($Characteristic = null, $Option = null, $Description = null)
     {
         parent::__construct();
 
         $this->setCharacteristic($Characteristic);
         $this->setOption($Option);
+        $this->setDescription($Description);
+    }
+
+    /**
+     * @param stdClass $json
+     *
+     * @return mixed|stdClass|null
+     *
+     * @throws InvalidArgumentException
+     * @throws NotSupportedException
+     *
+     * @since 1.2.0
+     */
+    public static function jsonDeserialize($json)
+    {
+        if (isset($json->ProductOption->CharacteristicCode)) {
+            $json->ProductOption->Characteristic = $json->ProductOption->CharacteristicCode;
+            unset($json->ProductOption->CharacteristicCode);
+        }
+
+        if (isset($json->ProductOption->OptionCode)) {
+            $json->ProductOption->Option = $json->ProductOption->OptionCode;
+            unset($json->ProductOption->OptionCode);
+        }
+
+        if (isset($json->ProductOption->OptionDescription)) {
+            $json->ProductOption->Description = $json->ProductOption->OptionDescription;
+            unset($json->ProductOption->OptionDescription);
+        }
+
+        return parent::jsonDeserialize($json);
     }
 }
